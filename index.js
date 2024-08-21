a1221e9a6d0478a914478eb912c4334cb44e52ce
larissagmz <larissagimenes.tinfem2023@gmail.com>
2024-08-21 12:19:00 -0300

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let listaProdutos = [
     {
+        id: 1,
         imagem: "./relogio1-removebg-preview.png",
         tipoProduto: "Relogios",
         titulo: "Fit Haiz",
@@ -8,6 +9,7 @@ let listaProdutos = [
         preco: 278.339,
     },
     {
+        id: 2,
         imagem: "./relogio2-removebg-preview.png",
         tipoProduto: "Relogios",
         titulo: "Ticwatch",
@@ -16,6 +18,7 @@ let listaProdutos = [
         preco: 100.339,
     },
     {
+        id: 3,
         imagem: "./relogio3-removebg-preview.png",
         tipoProduto: "Relogios",
         titulo: "TicWatch Pro 5",
@@ -24,6 +27,7 @@ let listaProdutos = [
         preco: 10.0,
     },
     {
+        id: 4,
         imagem: "./novocolar-removebg-preview.png",
         tipoProduto: "Colares",
         titulo: "Colar Sury",
@@ -32,6 +36,7 @@ let listaProdutos = [
         preco: 11.0,
     },
     {
+        id: 5,
         imagem: "./images-removebg-preview.png",
         tipoProduto: "Colares",
         titulo: "Colar Julia",
@@ -40,6 +45,7 @@ let listaProdutos = [
         preco: 13.0,
     },
     {
+        id: 6,
         imagem: "./novocolar-removebg-preview.png",
         tipoProduto: "Colares",
         titulo: "Colar Julia Dourado",
@@ -48,6 +54,7 @@ let listaProdutos = [
         preco: 10.0,
     },
     {
+        id: 7,
         imagem: "./camisa1-removebg-preview.png",
         tipoProduto: "Camisetas",
         titulo: "Poliéster",
@@ -56,6 +63,7 @@ let listaProdutos = [
         preco: 100,
     },
     {
+        id: 8,
         imagem: "./camiseta2-removebg-preview.png",
         tipoProduto: "Camisetas",
         titulo: "Dry Non-Wet",
@@ -64,6 +72,7 @@ let listaProdutos = [
         preco: 290,
     },
     {
+        id: 9,
         imagem: "./camisa3-removebg-preview.png",
         tipoProduto: "Camisetas",
         titulo: "Dry Non-Wet",
@@ -103,6 +112,7 @@ function renderizarCards(list) {
         preco.innerText = precoFormatado;
         let botao = document.createElement("button");
         botao.innerText = "Adicionar ao carrinho";
+        botao.className = "botao-comprar";
         liProdutos.append(figure, divInf);
         figure.appendChild(img);
         divInf.append(tipoProduto, titulo, descricao, preco, botao);
@@ -130,6 +140,114 @@ function separarCards(list) {
         }
     }
 }
-
-function separarCards(list) {}
 renderizarCards(listaProdutos);
+const divVitrine = document.querySelectorAll(".lista-produto");
+
+function acharItens() {
+    let form = document.querySelector(".formulario");
+    let botaoLista = document.querySelectorAll(".botao-comprar");
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let produtos = [];
+        divVitrine.forEach((ul) => {
+            ul.innerHTML = ""; // Limpa o conteúdo de cada <ul>
+        });
+        let input = document.querySelector("#barra-pesquisa");
+        if (input.value != "") {
+            for (let i = 0; i < listaProdutos.length; i++) {
+                if (listaProdutos[i].titulo === input.value) {
+                    produtos.push(listaProdutos[i]);
+                }
+            }
+            if (produtos.length > 0) {
+                renderizarCards(produtos);
+                adicionarCarrinho(produtos);
+                console.log(produtos);
+            }
+        } else renderizarCards(listaProdutos);
+    });
+}
+let carrinhoCompras = document.querySelector(".div-carrinho-vazio");
+
+function adicionarCarrinho(list) {
+    let botaoLista = document.querySelectorAll(".botao-comprar");
+    let produtos = [];
+    for (let i = 0; i < botaoLista.length; i++) {
+        botaoLista[i].setAttribute("data-id", list[i].id);
+    }
+    for (let i = 0; i < botaoLista.length; i++) {
+        botaoLista[i].addEventListener("click", (e) => {
+            carrinhoCompras.classList.remove("div-carrinho-vazio");
+            carrinhoCompras.classList.add("outro-carrinho");
+            let id = Number(botaoLista[i].getAttribute("data-id"));
+            for (let j = 0; j < list.length; j++) {
+                if (id === list[j].id) {
+                    produtos.push(list[j]);
+                }
+            }
+            carrinhoCompras.innerHTML = "";
+
+            cardsCarrinho(produtos);
+        });
+    }
+}
+
+function cardsCarrinho(list) {
+    let ul = document.createElement("ul");
+    ul.className = "lista-carrinho";
+    divInfCarrinho = document.querySelector(".carrinho-inf");
+    let pesquisa = document.querySelector(".pesquisa");
+    let precoTotal = 0;
+    let quantidadeTotal = 0;
+    for (let i = 0; i < list.length; i++) {
+        let precoFormatado = list[i].preco.toLocaleString("pt-br", {
+            style: "currency",
+            currency: "BRL",
+        });
+        precoTotal += list[i].preco;
+        quantidadeTotal += 1;
+        let li = document.createElement("li");
+        let figure = document.createElement("figure");
+        let img = document.createElement("img");
+        let div = document.createElement("div");
+        let titulo = document.createElement("titulo");
+        let preco = document.createElement("strong");
+        let botao = document.createElement("button");
+        li.className = "produto-carrinho";
+        div.className = "inf-produto-carrinho";
+        botao.className = "remover-carrinho";
+        img.src = list[i].imagem;
+        titulo.innerText = list[i].titulo;
+        preco.innerText = precoFormatado;
+        botao.innerText = "remover do carrinho";
+        carrinhoCompras.appendChild(ul);
+        ul.append(li);
+        li.append(figure, div);
+        figure.append(img);
+        div.append(titulo, preco, botao);
+
+        // let div1 = document.createElement("div");
+        // let quantidade = document.createElement("p");
+        // let total = document.createElement("p");
+        // let div2 = document.createElement("div");
+        // let textoQuantidade = document.createElement("strong");
+        // let textoPreco = document.createElement("strong");
+        // textoPreco.innerText = `${precoTotal}`;
+        // textoQuantidade.innerText = `${quantidadeTotal}`;
+
+        // divInfCarrinho.append(div1, div2);
+        // div1.append(quantidade, total);
+        // div2.append(textoQuantidade, textoPreco);
+
+        divInfCarrinho.innerHTML = "";
+        divInfCarrinho.innerHTML = `
+    <div class="quantidade-preco"><p>quantidade</p><p>total</p></div>
+    <div><strong>${quantidadeTotal}</strong><strong>${precoTotal}</strong></div>
+`;
+        pesquisa.append(divInfCarrinho);
+    }
+}
+
+adicionarCarrinho(listaProdutos);
+acharItens();
